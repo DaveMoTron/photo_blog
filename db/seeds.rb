2826d2d4e9aed16c5c42b656ca89d796ad59c2ee
thedav3 <dave@reinteractive.net>
2013-07-31 10:11:17 +0100

    adding seeds, removing old files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(
+  title: 'An example post of awesomeness',
+  location: 'Centennial Park, Sydney, Australia',
+  thumbnail_url: 'http://farm7.staticflickr.com/6165/6168661652_dd402c6a69_t.jpg',
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis tristique lacus, ut ornare lectus. Ut non vehicula felis. Nulla elementum in leo in lacinia. Donec tellus lectus, gravida nec sodales a, elementum eu arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras id malesuada orci. Suspendisse eleifend, velit nec pulvinar feugiat, augue risus adipiscing purus, eget volutpat eros tellus at justo. Pellentesque volutpat venenatis metus, eu placerat erat porttitor vitae. Aliquam sit amet ultricies risus. Fusce aliquam nunc nec aliquam laoreet. Etiam pretium, odio at euismod venenatis, massa augue tincidunt quam, vel dignissim diam ipsum ut sem. Aliquam convallis adipiscing tempus. Etiam vehicula mauris rhoncus enim tempor, vel vehicula nisl fermentum. Integer tincidunt risus non ligula interdum egestas.
+
+<a href='http://www.flickr.com/photos/davecruikshank/6168661652/' title='IMG_3211 by DaveMoTron, on Flickr' class='photo'><img src='http://farm7.staticflickr.com/6165/6168661652_7a3bb01087_o.jpg' alt='IMG_3211'></a>
+
+Aenean id pellentesque metus, in volutpat lorem. Mauris dictum sagittis leo, ac hendrerit lacus porttitor nec. Sed sit amet lorem commodo, imperdiet erat eu, molestie mauris. Morbi mollis orci est. Morbi accumsan turpis tellus, sollicitudin ullamcorper arcu placerat eu. Nullam pellentesque convallis venenatis. Maecenas fringilla id lorem ac lacinia. Vestibulum tristique velit non vestibulum feugiat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris eleifend, purus eu semper consectetur, orci libero facilisis metus, at dapibus nisl lorem id eros.
+
+<a href='http://www.flickr.com/photos/davecruikshank/6168661652/' title='IMG_3211 by DaveMoTron, on Flickr' class='photo third'><img src='http://farm7.staticflickr.com/6170/6168662214_9fd61671a4.jpg' alt='IMG_3207' class='third'></a>
+<a href='http://www.flickr.com/photos/davecruikshank/6168661652/' title='IMG_3211 by DaveMoTron, on Flickr' class='photo third'><img src='http://farm7.staticflickr.com/6153/6168127313_55c0746c17.jpg' alt='IMG_3185' class='third'></a>
+<a href='http://www.flickr.com/photos/davecruikshank/6168661652/' title='IMG_3211 by DaveMoTron, on Flickr' class='photo third'><img src='http://farm7.staticflickr.com/6163/6168664282_d221be52a6.jpg' alt='IMG_3208' class='third'></a>
+
+Nulla bibendum in quam id porta. Nunc ut aliquam odio. Aliquam molestie aliquet facilisis. Sed facilisis mauris non leo porta, vitae gravida dolor accumsan. Vestibulum eleifend lacus id eleifend sollicitudin. Nullam venenatis posuere tellus facilisis porta. Donec in massa quis magna egestas ullamcorper. Nullam laoreet ac libero in fringilla. Donec hendrerit aliquet metus, posuere rutrum nulla vestibulum et.
+
+Vestibulum a magna tellus. In hac habitasse platea dictumst. Aliquam accumsan urna elit, eget fermentum dui iaculis non. Suspendisse et rutrum arcu. Nulla suscipit in risus a dignissim. Vivamus est ante, sodales at ante eu, aliquet congue quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+<a href='http://www.flickr.com/photos/davecruikshank/6168661652/' title='IMG_3211 by DaveMoTron, on Flickr' class='photo half'><img src='http://farm7.staticflickr.com/6170/6168128867_05ae26fd6b.jpg' alt='IMG_3239' class='half'></a>
+<a href='http://www.flickr.com/photos/davecruikshank/6168661652/' title='IMG_3211 by DaveMoTron, on Flickr' class='photo half'><img src='http://farm7.staticflickr.com/6168/6168129639_399307634c.jpg' alt='IMG_3214' class='half'></a>
+
+Etiam sit amet congue nulla, eu malesuada nulla. Vestibulum varius, felis at gravida pulvinar, ipsum felis tristique mi, vel scelerisque elit libero ut tellus. Ut fermentum in felis sit amet vestibulum. Aliquam ut iaculis turpis. Ut faucibus nunc nec felis molestie pharetra. Cras dictum, nunc eget porttitor dignissim, ipsum nulla mattis nulla, at bibendum odio enim ac odio. Mauris nec felis id enim gravida convallis sit amet id sem. Vestibulum sit amet magna eget mi vulputate congue. Cras pulvinar enim at volutpat facilisis. Nam convallis dapibus velit, vitae cursus ligula auctor vel. Donec at lobortis mi, ultricies molestie neque. Nam egestas lectus a pulvinar vulputate. Vestibulum pulvinar rhoncus risus, non malesuada augue convallis eget. Phasellus vel elit diam.",
+  latitude: '-33.8942',
+  longitude: '151.239606',
+  published: true
+  )
