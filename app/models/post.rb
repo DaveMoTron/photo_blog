eccf3df8712e8aef0e331d42cb7b2c7377747f0a
thedav3 <dave@reinteractive.net>
2013-07-31 10:04:48 +0100

    styles for flickr links, adding published flag to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :location, :thumbnail_url, :body, :latitude, :longitude
-  validates_presence_of :title, :location, :thumbnail_url, :body, :latitude, :longitude
+  attr_accessible :title, :location, :thumbnail_url, :body, :latitude, :longitude, :published
+  validates_presence_of :title, :location, :thumbnail_url, :body
+  
   has_many :comments
+
+  scope :published, where(:published => true)
 end
